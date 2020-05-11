67275e54e485cbfbe4b1251d6a0c81f2fa11009f
Cornelius A Gregory <businessman7oh3@gmail.com>
2020-05-11 17:57:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
 console.log('Hello,World!');
 console.log('Hello,World!');
+let person1 = "Peter"
+let person2 = "Withnail"
+
+console.log( 'Hi, how are you, ' + person2 + '?' )
+console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
+
 
